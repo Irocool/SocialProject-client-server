598b01e5fbc1ea99ae60b100ebe0cbf0c1fb792e
Irocool <ikuligina@gmail.com>
2021-11-21 21:19:09 +0300

    Add pods

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SocialProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SocialProject
+  pod 'Alamofire', '~> 5.4'
+
+  target 'SocialProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SocialProjectUITests' do
+    # Pods for testing
+  end
+
+end
--- SocialProject/WebLoginViewController.swift
+++ SocialProject/WebLoginViewController.swift
@@ -15,6 +15,11 @@ class WebLoginViewController: UIViewController, WKNavigationDelegate{
             webKitView.navigationDelegate = self
         }
     }
+    private let appID = ""
+    private let versionAPI = "5.126"
+    private let scope = "336902"
+    private let redirectURI = "https://oauth.vk.com/blank.html"
+    private let responseURLpath = "/blank.html"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +27,44 @@ class WebLoginViewController: UIViewController, WKNavigationDelegate{
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadVKauthorization() {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: appID),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "scope", value: scope),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: versionAPI)
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        
+        webKitView.load(request)
     }
-    */
-
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url, url.path == responseURLpath, let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+        
+     //   let params = fragment
+     //       .components(separatedBy: "&")
+     //       .map { $0.components(separatedBy: "=") }
+     //       .reduce([String: String]()) { result, param in
+     //           var dict = result
+     //           let key = param[0]
+     //          let value = param[1]
+     //           dict[key] = value
+     //           return dict
+     //       }
+        
+        //let token = params["access_token"]
+       // let userID = params["user_id"]
+        
+    }
 }
